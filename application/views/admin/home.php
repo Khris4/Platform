835aff8b8e32ef9aa8971047ee6d582cf2dda647
Khris4 <khristiandisagala412@gmail.com>
2022-12-19 08:14:02 +0700

    projek

---
+++ application/views/admin/home.php
@@ -0,0 +1,49 @@
+<!-- <div class="grid">
+    <div class="title">
+        <center>
+            <span style="font-size:35px; margin-top:-20px"><b>SISTEM INFORMASI LAUNDRY</b></span><br>
+            <span style="font-size:40px;"><b><?= strtoupper($data->nama_laundry);?></b></span><br>
+            <span style="font-size:17px; font-style:italic;">alamat: <?= $data->alamat_laundry?> Tlp. <?= $data->no_tlp?></span>
+</center>
+    </div>
+</div> -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SD IT Wash</title>
+    <link rel="stylesheet" href="../assets/punyaku/css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="../assets/punyaku/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <!-- Navbar -->
+  <body>
+    <section class="home">
+      <div class="swiper bg-slider">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="../assets/punyaku/Image/slider1.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="../assets/punyaku/Image/slider2.png" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="../assets/punyaku/Image/slider3.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="bg-slider-thumbs">
+        <div class="swiper-wrapper thumbs-container">
+          <img src="../assets/punyaku/Image/slider1.png" class="swiper-slide" alt="" />
+          <img src="../assets/punyaku/Image/slider2.png" class="swiper-slide" alt="" />
+          <img src="../assets/punyaku/Image/slider3.png" class="swiper-slide" alt="" />
+        </div>
+      </div>
+    </section>
+    <!-- end Navbar -->
+
+    <script src="../assets/punyaku/js/swiper-bundle.min.js"></script>
+    <script src="../assets/punyaku/js/main.js"></script>
+  </body>
+</html>
